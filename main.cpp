74f71592aa8d9e173d6610573b2aaeeafd1f86a8
xtaci <daniel820313@gmail.com>
2016-07-09 17:51:57 +0800

    update

--- main.cpp
+++ main.cpp
@@ -8,16 +8,22 @@ int main() {
     gettimeofday(&time,NULL);
     srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
 
-    UDPSession *sess = UDPSession::Dial("127.0.0.1", 8888);
+    UDPSession *sess = UDPSession::Dial("127.0.0.1", 9999);
     int count;
+    char *buf = (char *)malloc(128);
     for (;;) {
+        sprintf(buf, "%d", count);
+        sess->Write(buf,strlen(buf));
         sess->Update();
+        memset(buf,0,128);
+        ssize_t n = sess->Read(buf,128);
+        printf("%s\n",buf);
+
         sleep(1);
         count++;
         if (count > 10) {
             break;
         };
-        std::cout << count<< std::endl;
     }
     sess->Close();
 }
