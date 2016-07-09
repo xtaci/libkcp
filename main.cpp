f1415957eafaf1d9e07bc0c4dfdba086581367cb
xtaci <daniel820313@gmail.com>
2016-07-09 17:00:06 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
+#include <unistd.h>
+#include <sys/time.h>
+#include "sess.h"
 #include <iostream>
-#include <sys/socket.h>
-#include <sys/fcntl.h>
-#include "ikcp.h"
-
-using namespace std;
 
 int main() {
+    struct timeval time;
+    gettimeofday(&time,NULL);
+    srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
 
+    UDPSession *sess = UDPSession::Dial("127.0.0.1", 8888);
+    int count;
+    for (;;) {
+        sess->Update();
+        sleep(1);
+        count++;
+        if (count > 10) {
+            break;
+        };
+        std::cout << count<< std::endl;
+    }
+    sess->Close();
 }
