8fe037fb2e4a9561cac013839cdb25aab501c554
xtaci <daniel820313@gmail.com>
2016-07-09 13:28:50 +0800

    update

--- sess.cpp
+++ sess.cpp
@@ -6,20 +6,22 @@
 #include <sys/socket.h>
 #include <sys/fcntl.h>
 #include <arpa/inet.h>
+#include <sys/time.h>
 #include <string.h>
 
 
-UDPSession * UDPSession::Dial(const char * ip, uint16_t port){
-    int sockfd = socket(PF_INET, SOCK_DGRAM ,0);
-    if(sockfd == -1){
+UDPSession *
+UDPSession::Dial(const char *ip, uint16_t port) {
+    int sockfd = socket(PF_INET, SOCK_DGRAM, 0);
+    if (sockfd == -1) {
         return 0;
     }
     int flags = fcntl(sockfd, F_GETFL, 0);
     fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
 
     struct sockaddr_in saddr;
-    saddr.sin_family    = AF_INET;
-    saddr.sin_port      = htons(port);
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = htons(port);
     saddr.sin_addr.s_addr = inet_addr(ip);
     memset(&saddr.sin_zero, 0, 8);
 
@@ -30,15 +32,44 @@ UDPSession * UDPSession::Dial(const char * ip, uint16_t port){
     return sess;
 }
 
-size_t UDPSession::PeekSize() {
-    return (size_t)ikcp_peeksize(this->m_kcp);
+void
+UDPSession::itimeofday(long *sec, long *usec)
+{
+#if defined(__unix)
+    struct timeval time;
+    gettimeofday(&time, NULL);
+    if (sec) *sec = time.tv_sec;
+    if (usec) *usec = time.tv_usec;
+#else
+    static long mode = 0, addsec = 0;
+    BOOL retval;
+    static IINT64 freq = 1;
+    IINT64 qpc;
+    if (mode == 0) {
+        retval = QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
+        freq = (freq == 0)? 1 : freq;
+        retval = QueryPerformanceCounter((LARGE_INTEGER*)&qpc);
+        addsec = (long)time(NULL);
+        addsec = addsec - (long)((qpc / freq) & 0x7fffffff);
+        mode = 1;
+    }
+    retval = QueryPerformanceCounter((LARGE_INTEGER*)&qpc);
+    retval = retval * 2;
+    if (sec) *sec = (long)(qpc / freq) + addsec;
+    if (usec) *usec = (long)((qpc % freq) * 1000000 / freq);
+#endif
 }
 
-int UDPSession::Read(char * buf, size_t sz) {
-    return ikcp_recv(this->m_kcp, buf, int(sz));
+IUINT64 UDPSession::iclock64(void) {
+    long s, u;
+    IUINT64 value;
+    itimeofday(&s, &u);
+    value = ((IUINT64) s) * 1000 + (u / 1000);
+    return value;
 }
 
-int UDPSession::Write(const char * buf, size_t sz) {
-    return ikcp_send(this->m_kcp, const_cast<char*>(buf), int(sz));
+IUINT32 UDPSession::iclock() {
+    return (IUINT32)(iclock64() & 0xfffffffful);
 }
+
 
