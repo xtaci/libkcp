dbaff7b044b8b8461ec4db84c0c0d84afdecce7e
xtaci <daniel820313@gmail.com>
2016-07-09 13:17:16 +0800

    update

--- sess.h
+++ sess.h
@@ -7,19 +7,40 @@
 
 #include "ikcp.h"
 
+#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+#include <windows.h>
+#elif !defined(__unix)
+#define __unix
+#endif
 
-class Session {
+
+class UDPSession {
 private:
     int m_sockfd;
-    ikcpcb *kcp;
+    ikcpcb *m_kcp;
 public:
-    static Session *DialKCP(char *network, char *address);
-    // Update pools udp socket and input into kcp;
-    int Update(char * buf, size_t sz);
+    static UDPSession *Dial(const char *ip, uint16_t port);
+
+    // Call Update() periodically for sending/receiving.
+    inline void Update() { ikcp_update(m_kcp, iclock()); }
+
     // PeekSize() returns the buffer size need to allocate for Read()
-    size_t PeekSize();
-    // Read reads from kcp and writes to buf and returns num of bytes written.
-    size_t Read(char * buf, size_t sz);
+    inline size_t PeekSize() { return (size_t) ikcp_peeksize(this->m_kcp); }
+
+    // Read reads from kcp with buffer size sz
+    inline int Read(char *buf, size_t sz) { return ikcp_recv(this->m_kcp, buf, int(sz)); }
+
+    // Write writes into kcp with buffer size sz
+    inline int Write(const char *buf, size_t sz) { return ikcp_send(this->m_kcp, const_cast<char *>(buf), int(sz)); }
+
+private:
+    void itimeofday(long *sec, long *usec);
+
+    /* get clock in millisecond 64 */
+    int64_t iclock64();
+
+    /* get clock in millisecond 32 */
+    int32_t iclock();
 };
 
 #endif //KCP_SESS_H
