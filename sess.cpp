1b4a77e97dcd48722706c6075e2420580144cce7
xtaci <daniel820313@gmail.com>
2016-07-09 12:58:59 +0800

    update

--- sess.cpp
+++ sess.cpp
@@ -5,10 +5,11 @@
 #include "sess.h"
 #include <sys/socket.h>
 #include <sys/fcntl.h>
-#include <stdlib.h>
+#include <arpa/inet.h>
+#include <string.h>
 
 
-Session * Session::DialKCP(char * network, char * address){
+UDPSession * UDPSession::Dial(const char * ip, uint16_t port){
     int sockfd = socket(PF_INET, SOCK_DGRAM ,0);
     if(sockfd == -1){
         return 0;
@@ -16,14 +17,28 @@ Session * Session::DialKCP(char * network, char * address){
     int flags = fcntl(sockfd, F_GETFL, 0);
     fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
 
+    struct sockaddr_in saddr;
+    saddr.sin_family    = AF_INET;
+    saddr.sin_port      = htons(port);
+    saddr.sin_addr.s_addr = inet_addr(ip);
+    memset(&saddr.sin_zero, 0, 8);
 
-
-    Session *sess = new(Session);
+    UDPSession *sess = new(UDPSession);
     sess->m_sockfd = sockfd;
-    sess->kcp = ikcp_create(rand(), sess);
+    sess->m_kcp = ikcp_create(IUINT32(rand()), sess);
 
     return sess;
 }
 
+size_t UDPSession::PeekSize() {
+    return (size_t)ikcp_peeksize(this->m_kcp);
+}
 
+int UDPSession::Read(char * buf, size_t sz) {
+    return ikcp_recv(this->m_kcp, buf, int(sz));
+}
+
+int UDPSession::Write(const char * buf, size_t sz) {
+    return ikcp_send(this->m_kcp, const_cast<char*>(buf), int(sz));
+}
 
