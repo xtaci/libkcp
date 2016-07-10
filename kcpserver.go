8f0dcd6c6ec2dccee5f3f8482efc19f1fb88b433
xtaci <daniel820313@gmail.com>
2016-07-10 22:24:47 +0800

    add test server

---
+++ kcpserver.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/xtaci/kcp-go"
+)
+
+const port = ":9999"
+
+func ListenTest() (*kcp.Listener, error) {
+	return kcp.ListenWithOptions(port, nil, 1, 0)
+}
+
+func server() {
+	l, err := ListenTest()
+	if err != nil {
+		panic(err)
+	}
+	for {
+		s, err := l.Accept()
+		if err != nil {
+			panic(err)
+		}
+
+		go handle_client(s)
+	}
+}
+func handle_client(conn *kcp.UDPSession) {
+	conn.SetWindowSize(1024, 1024)
+	conn.SetNoDelay(1, 20, 2, 1)
+	fmt.Println("new client", conn.RemoteAddr())
+	buf := make([]byte, 65536)
+	count := 0
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			panic(err)
+		}
+		count++
+		fmt.Println("received:", buf[:n])
+		conn.Write(buf[:n])
+	}
+}
+
+func main() {
+	server()
+}
