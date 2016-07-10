59619a396d31174a303c4a20b799cea2fae69828
xtaci <daniel820313@gmail.com>
2016-07-10 18:26:20 +0800

    update

--- main.cpp
+++ main.cpp
@@ -3,18 +3,21 @@
 #include "sess.h"
 #include <iostream>
 
+IUINT32 iclock();
+
 int main() {
     struct timeval time;
     gettimeofday(&time,NULL);
     srand((time.tv_sec * 1000) + (time.tv_usec / 1000));
 
-    UDPSession *sess = UDPSession::Dial("127.0.0.1", 9999);
+    UDPSession *sess = UDPSession::DialIPv6("::1", 9999);
+    assert(sess!= nullptr);
     int count;
     char *buf = (char *)malloc(128);
     for (;;) {
         sprintf(buf, "%d", count);
         sess->Write(buf,strlen(buf));
-        sess->Update();
+        sess->Update(iclock());
         memset(buf,0,128);
         ssize_t n = sess->Read(buf,128);
         printf("%s\n",buf);
@@ -27,3 +30,26 @@ int main() {
     }
     sess->Close();
 }
+
+
+void
+itimeofday(long *sec, long *usec) {
+    struct timeval time;
+    gettimeofday(&time, NULL);
+    if (sec) *sec = time.tv_sec;
+    if (usec) *usec = time.tv_usec;
+}
+
+IUINT64 iclock64(void) {
+    long s, u;
+    IUINT64 value;
+    itimeofday(&s, &u);
+    value = ((IUINT64) s) * 1000 + (u / 1000);
+    return value;
+}
+
+IUINT32 iclock() {
+    return (IUINT32) (iclock64() & 0xfffffffful);
+}
+
+
