a45b3ae525c9a1fc0528f69d4187af1842cd92a8
xtaci <daniel820313@gmail.com>
2016-12-30 18:43:39 +0800

    upd

--- galois_noasm.h
+++ galois_noasm.h
@@ -7,7 +7,14 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 void galMulSlice(uint8_t c, uint8_t in[], uint8_t out[], size_t sz);
 void galMulSliceXor(uint8_t c, uint8_t in[], uint8_t out[],size_t sz);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //KCP_GALOIS_NOASM_H
