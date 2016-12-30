6b7fd4d484a4130f642b111fd618cd04c9dd772e
xtaci <daniel820313@gmail.com>
2016-12-30 18:42:16 +0800

    upd

--- matrix.cpp
+++ matrix.cpp
@@ -4,8 +4,8 @@
 
 #include <cstdlib>
 #include <cstring>
+#include "galois.h"
 #include "matrix.h"
-
 
 matrix *
 matrix::newMatrix(int rows, int cols) {
@@ -14,6 +14,8 @@ matrix::newMatrix(int rows, int cols) {
     }
 
     matrix *m = new(matrix);
+    m->rows = rows;
+    m->cols = cols;
     m->data = (uint8_t **) malloc(sizeof(uintptr_t) * rows);
     for (int i = 0; i < rows; i++) {
         m->data[i] = (uint8_t *) malloc(sizeof(uint8_t) * cols);
@@ -23,7 +25,7 @@ matrix::newMatrix(int rows, int cols) {
 }
 
 matrix *matrix::identityMatrix(int size) {
-    matrix *m = newMatrix(size, size);
+    matrix *m = matrix::newMatrix(size, size);
     if (m == nullptr) {
         return nullptr;
     }
@@ -33,4 +35,26 @@ matrix *matrix::identityMatrix(int size) {
     }
     return m;
 }
+
+
+matrix *matrix::Multiply(matrix *right) {
+    if (this->rows != right->cols) {
+        return nullptr;
+    }
+
+    matrix * result = matrix::newMatrix(this->rows,right->cols);
+
+    for (int r=0;r<result->rows;r++) {
+        for (int c=0; c< result->cols;c++) {
+            uint8_t value = 0;
+            for (int i=0; i<this->cols;i++) {
+                value ^= galMultiply(this->data[r][i], right->data[i][c]);
+            }
+            result->data[r][c] = value;
+        }
+    }
+    return result;
+}
+
+
 
--- matrix.h
+++ matrix.h
@@ -28,6 +28,7 @@ public:
     matrix *Multiply(matrix *right);
 
 private:
+    int rows,cols;
     matrix() = default;
 
     ~matrix() = default;
