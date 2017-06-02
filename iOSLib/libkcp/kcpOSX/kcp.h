cc9e950a5ec413fed9bbef11ca72010300e1749f
Kong XiangBo <yarshure@gmail.com>
2017-06-02 23:15:38 +0800

    update macOS target

--- iOSLib/libkcp/kcpOSX/kcp.h
+++ iOSLib/libkcp/kcpOSX/kcp.h
@@ -17,3 +17,5 @@ FOUNDATION_EXPORT const unsigned char kcpOSXVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <kcpOSX/PublicHeader.h>
 
 
+#import <kcp/Tunconfig.h>
+#import <kcp/SFKcptun.h>
